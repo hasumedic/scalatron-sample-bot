65f83e46d0f00b779d36372adf9b75d9bc7e1ed1
Alex Salguero <hasumedic@gmail.com>
2016-02-13 13:05:07 +0000

    Commenting tests

--- src/test/scala/co/uk/hackthetower/exercises/Ex2BotLogicSpec.scala
+++ src/test/scala/co/uk/hackthetower/exercises/Ex2BotLogicSpec.scala
@@ -7,10 +7,10 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Ex2BotLogicSpec extends FlatSpec with Matchers {
 
-  "processServerCommand" should "return Left if Goodbye is received with a large negative number as we can't lose, obviously it's a mistake!" in {
-    val input = Xor.right(Goodbye(-10000))
-    Ex2BotLogic.processServerCommand(input).isLeft should be(true)
-  }
+//  "processServerCommand" should "return Left if Goodbye is received with a large negative number as we can't lose, obviously it's a mistake!" in {
+//    val input = Xor.right(Goodbye(-10000))
+//    Ex2BotLogic.processServerCommand(input).isLeft should be(true)
+//  }
 
   //TODO: add tests for your logic!
 }
